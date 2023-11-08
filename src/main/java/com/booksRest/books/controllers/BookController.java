ad27bbc75c83b82ae9351159035369141eec6d2e
dineshWic <109039671+dineshWic@users.noreply.github.com>
2023-11-09 00:40:04 +0530

    update,delete and get routes

---
+++ src/main/java/com/booksRest/books/controllers/BookController.java
@@ -0,0 +1,57 @@
+package com.booksRest.books.controllers;
+
+import com.booksRest.books.domain.Book;
+import com.booksRest.books.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(final BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping(path = "/books/{isbn}")
+    public ResponseEntity<Book> createBook(@PathVariable final String isbn, @RequestBody final Book book){
+        book.setIsbn(isbn);
+
+        final boolean isBookExists = bookService.isBookExists(book);
+        final Book savedBook = bookService.save(book);
+
+        if(isBookExists) return new ResponseEntity<Book>(savedBook, HttpStatus.OK);
+        else {
+            ResponseEntity<Book> response = new ResponseEntity<>(savedBook, HttpStatus.CREATED);
+            return response;
+        }
+    }
+
+    @GetMapping(path = "books/{isbn}")
+    public ResponseEntity<Book> retrieveBook(@PathVariable final String isbn){
+        final Optional<Book> foundBook = bookService.findById(isbn);
+        return foundBook
+                .map(book -> new ResponseEntity<Book>(book, HttpStatus.OK))
+                .orElse(new ResponseEntity<Book>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping(path = "/books")
+    public ResponseEntity<List<Book>> listBooks(){
+        return new ResponseEntity<List<Book>>(bookService.listBooks(),HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/books/{isbn}")
+    public ResponseEntity deleteBook(@PathVariable final String isbn) {
+        bookService.deleteBookById(isbn);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
--- src/main/java/com/booksRest/books/services/impl/BookServiceImpl.java
+++ src/main/java/com/booksRest/books/services/impl/BookServiceImpl.java
@@ -4,10 +4,17 @@ import com.booksRest.books.domain.Book;
 import com.booksRest.books.domain.BookEntity;
 import com.booksRest.books.repositories.BookRepository;
 import com.booksRest.books.services.BookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
+@Slf4j
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
@@ -18,11 +25,39 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public Book create(final Book book) {
+    public Book save(final Book book) {
         final BookEntity bookEntity = bookToBookEntity(book);
         final BookEntity savedBookEntity = bookRepository.save(bookEntity);
         return bookEntityToBook(savedBookEntity);
     }
+
+    @Override
+    public Optional<Book> findById(String id) {
+        final Optional<BookEntity> foundBook = bookRepository.findById(id);
+        return foundBook.map(book -> bookEntityToBook(book));
+    }
+
+    @Override
+    public List<Book> listBooks() {
+        final List<BookEntity> foundBooks = bookRepository.findAll();
+        return foundBooks.stream().map(book -> bookEntityToBook(book)).collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteBookById(String isbn) {
+        try {
+            bookRepository.deleteById(isbn);
+        }
+        catch (EmptyResultDataAccessException ex){
+            log.debug("Attempt to delete empty data"+ex);
+        }
+    }
+
+    @Override
+    public boolean isBookExists(Book book){
+        return bookRepository.existsById(book.getIsbn());
+    }
+
 
     private BookEntity bookToBookEntity(Book book){
         return BookEntity.builder()
@@ -39,5 +74,9 @@ public class BookServiceImpl implements BookService {
                 .title(bookEntity.getTitle())
                 .build();
     }
+
+
+
+
 
 }
